3602c7efd09ff541f909419ed653bfeaf9e0b03d
Daynya Quigley <daynya@gmail.com>
2019-06-13 20:36:48 -0400

    Continued JavaScript

--- scripts.js
+++ scripts.js
@@ -1,18 +1,47 @@
 const cards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let fistCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    if (lockBoard)
+    this.classList.add('flip');
 
     if (!hasFlippedCard) {
-        //first click
+        //if hasFlippedCard is false, this is the first click
         hasFlippedCard = true;
         firstCard = this;
 
-        console.log({hasFlippedCard, firstCard});
-    }
+        return;
+    } 
+    
+        //if hasFlippedCard is true, this is the second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        checkForMatch();
+}
+
+function checkForMatch() {
+    //do the cards match?
+    //if the dataset matches on both cards, remove event listener to prevent cards from being clicked again
+    let isMatch = firstCard.dataset.animal === secondCard.dataset.animal;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+    //if the datasets do not match, flip cards back to original state
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
